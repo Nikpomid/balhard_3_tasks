df23004a1e05d73588305e5a0dd209ec0ece4866
Timofey Silivonchyk <silivon4iktima@icloud.com>
2023-02-10 14:58:05 +0300

    fio.py, multiply.py, two_parts, sumstr.py  complite

--- tasks/strings/sumstr.py
+++ tasks/strings/sumstr.py
@@ -19,7 +19,7 @@ def sum_str(first_str: str, second_str: str) -> str:
 
     :return: результат склеивания
     """
-    result = None
+    result = str(first_str + ' ' + second_str)
     return result
 
 
